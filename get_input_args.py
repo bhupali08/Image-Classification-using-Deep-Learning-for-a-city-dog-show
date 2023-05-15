81b3a3a04f1686f8a527000608c8acf5d7960213
Soham Bera <65274128+SohamBera16@users.noreply.github.com>
2023-05-16 00:01:23 +0200

    Update get_input_args.py

--- get_input_args.py
+++ get_input_args.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
 #                                                                             
-# PROGRAMMER: Bhupali
+# PROGRAMMER: Soham
 # DATE CREATED:  26/11/2022                                 
 # REVISED DATE: 
 # PURPOSE: Create a function that retrieves the following 3 command line inputs 
